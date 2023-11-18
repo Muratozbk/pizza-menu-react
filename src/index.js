fb1ca76aed213a7a35735420fcfcf52636bc08ca
muratozbk <slipstream11@yandex.com>
2023-11-18 20:15:39 +0300

    pizza comp

--- src/index.js
+++ src/index.js
@@ -1,22 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
 
 function App() {
-    return <div>
-        <h1>Hello react!</h1>
-        <Pizza />
-        <Pizza />
+    return <div className="container">
+        <Header />
+        <Menu />
+        <Footer />
     </div>
 }
 
-function Pizza() {
+function Header() {
     return (
-        <div>
-            <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-            <h2>pizza Spinaci!</h2>
+        <header className="header">
+            <h1 > Fast React Pizza Co.</h1>
+        </header>
+    )
+};
+
+function Menu() {
+    return (
+        <main className="menu" >
+            <h2>Our menu</h2>
+            <Pizza
+                name='Pizza Spinaci'
+                ingredients='Tomato, mozarella, spinach and ricotta cheese'
+                photoName="pizzas/spinaci.jpg"
+                price={10} />
+
+            <Pizza
+                name="Pizza Funghi"
+                ingredients='Tomato, mushrooms'
+                price={12}
+                photoName='pizzas/funghi.jpg' />
+        </main>
+    )
+};
+
+function Pizza(props) {
+    return (
+        <div className="pizza">
+            <img src={props.photoName} alt={props.name} />
+            <div>
+                <h3>{props.name} </h3>
+                <p>{props.ingredients} </p>
+                <span>{props.price} </span>
+            </div>
         </div>
     )
 };
+
+function Footer() {
+    return (
+        <footer className="footer">
+            {new Date().toLocaleTimeString()}.  We're currently open
+        </footer>
+    )
+};
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
