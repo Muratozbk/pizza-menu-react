707f3392094cf588b9bbd4369b56d557eb152d46
muratozbk <slipstream11@yandex.com>
2023-11-21 21:10:04 +0300

    finish project

--- src/index.js
+++ src/index.js
@@ -66,6 +66,7 @@ function Header() {
 
 function Menu() {
     const pizzas = pizzaData;
+    // const pizzas = [];
     const numPizzas = pizzas.length;
 
     return (
@@ -73,11 +74,14 @@ function Menu() {
             <h2>Our menu</h2>
 
             {numPizzas > 0 ? (
-                <ul className="pizzas">
-                    {pizzas.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name} />
-                    ))}
-                </ul>
+                <>
+                    <p>Authentic Italian cuisine. Many creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+                    <ul className="pizzas">
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
             ) : (<p>We're still working on our menu. Please come back later. </p>)}
         </main>
     )
@@ -85,15 +89,15 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
 
-    if (pizzaObj.soldOut) return null;
+    // if (pizzaObj.soldOut) return null;
 
     return (
-        <li className="pizza">
+        <li className={`pizza ${pizzaObj.soldOut && 'sold-out'}`}>
             <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
                 <h3>{pizzaObj.name} </h3>
                 <p>{pizzaObj.ingredients} </p>
-                <span>{pizzaObj.price} </span>
+                <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price} </span>
             </div>
         </li>
     )
